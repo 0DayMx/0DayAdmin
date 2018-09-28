88ea7722a3d753ad7bcd5539b5a01dac3f1c0600
Milton Ortega <MIGMANMX@users.noreply.github.com>
2018-09-28 11:18:25 -0500

    01

---
+++ 0DayAdmin/Login.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Behaviours;
+
+
+namespace _0DayAdmin
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btningresar_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (txtusuario.Text == "admin" && pbcontraseña.Password == "123")
+            {
+                await this.ShowMessageAsync("Exito", "Tus datos son correctos");
+                Menu _ver = new Menu();
+                this.Close();
+                _ver.ShowDialog();
+            }
+            else
+            {
+                await this.ShowMessageAsync("Error!", "Verifica tus datos");
+            }
+        }
+    }
+}
---
+++ 0DayAdmin/Menu.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Behaviours;
+
+namespace _0DayAdmin
+{
+    /// <summary>
+    /// Lógica de interacción para Menu.xaml
+    /// </summary>
+    public partial class Menu : MetroWindow
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Tile_Click_1(object sender, RoutedEventArgs e)
+        {
+            flyprueba.IsOpen = true;
+        }
+
+        private void Tile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            await this.ShowMessageAsync("No te pierdas el siguiente tutorial :D", "Por favor no olvides suscribirte y darle like si te ha gustado el video...");
+
+        }
+    }
+}
